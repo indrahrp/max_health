1f5907568cd270a6acb9e0f4aa69c4e276020f54
indragendx <iharahap@genedx.com>
2026-03-22 22:28:03 -0500

    switch to Supabase PostgreSQL

--- max_health/settings.py
+++ max_health/settings.py
@@ -57,11 +57,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'max_health.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
